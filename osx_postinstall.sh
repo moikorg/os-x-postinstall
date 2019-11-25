7511d2fc544634dc3e32b84b1b355b6b9f673e40
Mike <moikorg@users.noreply.github.com>
2019-11-25 10:27:32 +0100

    cyberduck added to the cask list

--- osx_postinstall.sh
+++ osx_postinstall.sh
@@ -45,7 +45,7 @@ function cask_base_packages {
   # Install base cask packages
   brew cask install google-chrome spotify beyond-compare docker iterm2 dashlane dbvisualizer 
   brew cask install bartender little-snitch nextcloud path-finder pycharm sourcetree vmware-fusion bettertouchtoolx
-  brew cask install wireshark
+  brew cask install cyberduck wireshark
 }
 
 function mas_install {
